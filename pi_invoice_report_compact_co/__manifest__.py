f3f448aa355fff71eea7111b41e3948ac75a16f3
Andres Pineda <apineda099@gmail.com>
2026-04-23 12:09:01 -0500

    feat: :sparkles: Add module to improve invoice PDF

---
+++ pi_invoice_report_compact_co/__manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Reporte Compacto Factura Electrónica CO',
+    'version': '18.0.1.0.0',
+    'author': 'Andres Pineda',
+    'summary': 'Reporte compacto de facturación electrónica colombiana, maximiza el uso del espacio de página',
+    'depends': ['l10n_co_edi_jorels'],
+    'data': [
+        'report/report_actions.xml',
+        'report/report_invoice_compact.xml',
+    ],
+    'application': False,
+    'installable': True,
+    'license': 'LGPL-3',
+}
